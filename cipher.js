78c0e6c60e7892fcda7a3ab39096d5780aa68938
Jeannie Fallon <jeanniefallon@gmail.com>
2017-12-25 16:08:43 -0500

    begin caesar algorithm

--- cipher.js
+++ cipher.js
@@ -5,7 +5,28 @@ function clearKey(id) {
 function goCaesar(txtId, keyId) {
   var plnStr = document.getElementById(txtId).value;
   var cphStr = "";
+
   var key = document.getElementById(keyId).value;
+
+  var currChar = "";
+
+  for (var i = 0; i < plnStr.length; i++) {
+    currChar = plnStr[i];
+
+    var chCd = plnStr.charCodeAt(i);
+    console.log(chCd);
+
+    var newCd = chCd + parseInt(key);
+
+    console.log(newCd);
+
+    var newChar = String.fromCharCode(newCd);
+
+    console.log(newChar);
+    console.log("break");
+
+    cphStr += currChar;
+  }
 
   document.getElementById('ciphertext').innerHTML = cphStr;
 }
