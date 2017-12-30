6d970ef1835df9d076bebd590ae7a53493f083bb
Jeannie Fallon <jeanniefallon@gmail.com>
2017-12-30 17:51:36 -0500

    caesar algorithm basic functionality

--- cipher.js
+++ cipher.js
@@ -2,33 +2,26 @@ function clearKey(id) {
   document.getElementById(id).value = "";
 }
 
-function getAlphaIdx(code) {
-  //TODO add ASCII range for upper & lower
-  var idx = 0;
-  if (true) {
-    idx = code - 97;
-  } else {
-    idx = code - 65;
-  }
-  //TODO set & return bool isLower
-  return idx;
+function getIdxLower(code) {
+  return code - 97;
 }
 
-function getNewIdx(idx, key) {
+function getIdxUpper(code) {
+  return code - 65;
+}
+
+function getCodeLower(idx) {
+  return idx + 97;
+}
+
+function getCodeUpper(idx) {
+  return idx + 65;
+}
+
+function getRotIdx(idx, key) {
   var newIdx = (idx + key) % 26;
+  return newIdx;
 }
-
-function getCharCode(idx) {
-  //TODO how to preserve upper/lowercase? add a bool?
-  var isLower = true;
-  if (isLower) {
-    var newCd = idx + 97;
-  } else {
-    var newCd = idx + 65;
-  }
-  return newCd;
-}
-
 
 function goCaesar(txtId, keyId) {
   var plnStr = document.getElementById(txtId).value;
@@ -36,26 +29,31 @@ function goCaesar(txtId, keyId) {
   var key = parseInt(document.getElementById(keyId).value);
   var currChar = "";
   var newChar = "";
-  var currCd = 0;
+  var code = 0;
   var newCd = 0;
   var currIdx = 0;
   var newIdx = 0;
+  var isLower = false;
 
   for (var i = 0; i < plnStr.length; i++) {
 
     //TODO test if alpha char
     if (true) {
-      currChar = plnStr[i];
-      chCd = plnStr.charCodeAt(i);
-      currIdx = getAlphaIdx(chCd);
-      newIdx = getNewIdx(currIdx, key);
-      newCd = getCharCode(newIdx);
+      code = plnStr.charCodeAt(i);
+      if (code >= 97 && code <= 122) {
+        currIdx = getIdxLower(code);
+        newIdx = getRotIdx(currIdx, key);
+        newCd = getCodeLower(newIdx);
+      } else {
+        currIdx = getIdxUpper(code);
+        newIdx = getRotIdx(currIdx, key);
+        newCd = getCodeUpper(newIdx);
+      }
       newChar = String.fromCharCode(newCd);
       cphStr += newChar;
     } else {
       cphStr += currChar;
     }
   }
-  console.log(cphStr);
   document.getElementById('ciphertext').innerHTML = cphStr;
 }
